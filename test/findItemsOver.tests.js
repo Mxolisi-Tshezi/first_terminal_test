88bcf68b2894cbe09c6e8186a1039785c2156e50
Mxolisi <nyawose57@gmail.com>
2022-07-18 16:01:21 +0200

    saving latest chnges to testfiles

--- test/findItemsOver.tests.js
+++ test/findItemsOver.tests.js
@@ -1,23 +1,23 @@
-// let assert = require("assert");
-// let findItemsOver = require("../findItemsOver");
+// // let assert = require("assert");
+// // let findItemsOver = require("../findItemsOver");
 
-// describe('findItemsOver' , function(){
-//     it('quantity higher than the threshold' , function(){
-//         assert.deepEqual(findItemsOver(itemList, threshold), results);
-//     })
+// // describe('findItemsOver' , function(){
+// //     it('quantity higher than the threshold' , function(){
+// //         assert.deepEqual(findItemsOver(itemList, threshold), results);
+// //     })
   
 
-// });
+// // });
 
 
 
-var itemList = [
-    {name : 'apples', qty : 10},
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
-    {name : 'apples', qty : 3},
-];
+// var itemList = [
+//     {name : 'apples', qty : 10},
+//     {name : 'pears', qty : 37},
+//     {name : 'bananas', qty : 27},
+//     {name : 'apples', qty : 3},
+// ];
 
-var threshold = 25;
-   var results = findItemsOver(itemList, 25)
-  //  console.log(results)
+// var threshold = 25;
+//    var results = findItemsOver(itemList, 25)
+//   //  console.log(results)
